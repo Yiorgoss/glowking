621af7a86815946b219aa68fb0f53ca674a72e63
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2023-02-07 19:54:10 +0200

    ** Refactored responsive header
    added sitemap to layout removed from header and footer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,96 +4,98 @@ import { useRef, useState, useEffect } from "react";
 
 import { AiOutlineMenu } from "react-icons/ai";
 
-const NAV_LINKS = [
-  {
-    title: "Home",
-    path: "/",
-  },
-  {
-    title: "About Us",
-    path: "/about-us",
-  },
-  {
-    title: "Services",
-    path: "/Services",
-  },
-  {
-    title: "Testimonials",
-    path: "/testimonials",
-  },
-  {
-    title: "Contact",
-    path: "/contact",
-  },
-  {
-    title: "Contact",
-    path: "/contact",
-  },
-  {
-    title: "Contact",
-    path: "/contact",
-  },
-  {
-    title: "Contact",
-    path: "/contact",
-  },
-];
-export default function Header(): JSX.Element {
-  const node = useRef<HTMLDivElement>(null);
-  const [open, setOpen] = useState<boolean>(false);
+import { navLinkType } from "@cTypes/inputTypes";
+import Divider from "./divider";
 
-  //const handleClickOutside = (e: MouseEvent) => {
-  //  if (node.current && node.current.contains(e.target as Node)) {
-  //    // inside click
-  //    return;
-  //  }
-  //  // outside click
-  //  setOpen(false);
-  //};
-
-  //useEffect(() => {
-  //  if (open) {
-  //    document.addEventListener("mousedown", handleClickOutside);
-  //  } else {
-  //    document.removeEventListener("mousedown", handleClickOutside);
-  //  }
-
-  //  return () => {
-  //    document.removeEventListener("mousedown", handleClickOutside);
-  //  };
-  //}, [open]);
-
-  return (
-    <div className="w-full">
-      <div className=" flex h-[200px] flex-wrap justify-between overflow-hidden bg-black">
-        <div className="text-md my-auto mr-auto pl-10 text-white ">
-          {" "}
-          691 111 1111{" "}
-        </div>
-        <div className="w-[100px] bg-red-400"> logo </div>
-        <div className="m-auto basis-full text-center text-white">
-          {" "}
-          * Dry * Buff * Vacuum * Valet *{" "}
-        </div>
-      </div>
-      <div className="bg-red-700">
-        <div className="py-1" onClick={() => setOpen(!open)}>
-          <AiOutlineMenu className="mx-auto text-white" />
-        </div>
-        <div className={`${open && "hidden"} h-full bg-red-400 text-white`}>
-          <ul>
-            {NAV_LINKS.map((link, index) => (
-              <li className=" ml-14 bg-red-200 pt-2" key={index}>
-                {" "}
-                <Link href={link.path} onClick={() => setOpen(!open)}>
-                  {" "}
-                  {link.title}{" "}
-                </Link>{" "}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+interface headerProps {
+    navLinks: navLinkType[];
 }
+export default function Header({ navLinks }: headerProps): JSX.Element {
+    //TODO: refactor and split code up
+    //TODO: add clickListener to close on surrounding click
+    //TODO: refactor the different headings for responsive and normal view
+
+    const [isOpen, setIsOpen] = useState<boolean>(true);
+
+    return (
+        <div className="grid grid-cols-2 md:grid-cols-3 items-center justify-items-center">
+            <div className="text-xl md:text-5xl font-bold">69 123 456 78</div>
+            <div className="py-2 pt-2 text-lg font-medium md:col-auto col-span-full">* snappy * motto * or * TLDR *</div>
+            <div className="relative m-5 h-[150px] w-[150px] row-start-1 col-start-2 md:row-start-auto md:col-start-auto">
+                <Image
+                    className="h-auto w-auto"
+                    src="https://via.placeholder.com/150"
+                    alt="logo"
+                    fill
+                />
+            </div>
+            <ul className="col-span-full my-3 text-2xl">
+                <div className="py-1 md:hidden" onClick={() => setIsOpen(!isOpen)}>
+                    <AiOutlineMenu className="mx-auto font-black text-secondary text-2xl" />
+                </div>
+                {navLinks.map((link, index) => (
+                  <li className={` ${isOpen && 'hidden'} md:block md:float-left mx-3 p-3 hover:text-primary hover:bg-secondary`} key={index}>
+                        <Link
+                            className="after:content-['.'] hover:after:w-3/4 after:transition-width after:h-[2px] relative p-2 after:absolute after:left-0 after:right-0 after:bottom-0 after:m-auto after:w-0 after:bg-primary after:text-transparent after:duration-500"
+                            href={link.path}
+                            onClick={() => setIsOpen(!isOpen)}
+                        >
+                            {link.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+//         <div className="relative top-0 flex w-full flex-wrap ">
+//            <div className=" flex h-[200px] w-full flex-wrap justify-between bg-primary">
+//                <div className="text-md my-auto text-2xl mr-auto pl-10 text-secondary ">
+//                    {" "}
+//                    691 111 1111{" "}
+//                </div>
+//                <Image
+//                    className="mr-10 mt-5"
+//                    src="https://via.placeholder.com/150.png?text=glowking.com"
+//                    alt="logo"
+//                    height={150}
+//                    width={150}
+//                />
+//                <div className="m-auto basis-full text-center text-secondary">
+//                    {" "}
+//                    * Dry * Buff * Vacuum * Valet *{" "}
+//                </div>
+//            </div>
+//            <div className="basis-full bg-secondary md:hidden">
+//                <div className="py-1" onClick={() => setOpen(!open)}>
+//                    <AiOutlineMenu className="mx-auto text-primary" />
+//                </div>
+//                <div className="sm:hidden w-full relative">
+//                  <div className="">
+//                    <ul>
+//                        {navLinks.map((link, index) => (
+//                            <li className="" key={index}>
+//                                <Link className=" ml-14 pt-2" href={link.path} onClick={() => setOpen(!open)}>
+//                                    {link.title}
+//                                </Link>
+//                            </li>
+//                        ))}
+//                    </ul>
+//
+//                  </div>
+//                </div>
+//                <div className={`${open && "hidden"} h-full my-5 text-primary`}>
+//                    <ul>
+//                        {navLinks.map((link, index) => (
+//                            <li className="" key={index}>
+//                                <Divider color='primary' />
+//                                <Link className=" ml-14 pt-2" href={link.path} onClick={() => setOpen(!open)}>
+//                                    {link.title}
+//                                </Link>
+//                            </li>
+//                        ))}
+//</ul>
+//</div>
+//</div>
+//</div>
+//
--- src/layouts/headerLayout.tsx
+++ src/layouts/headerLayout.tsx
@@ -1,20 +1,46 @@
-import Header from "@components/header"
+import NewsletterForm from "@/components/newsletterform"
+import Footer from '@/components/footer'
+import Header from "@/components/header"
 import { ReactElement } from "react"
 
 interface HeaderLayoutProps {
     children?: ReactElement|ReactElement[]
 }
 
+const NAV_LINKS = [
+  //TODO: correct navlinks
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "Services",
+    path: "/services",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Portfolio",
+    path: "/portfolio",
+  },
+];
+
 export default function HeaderLayout({ children }: HeaderLayoutProps) {
     return (
-        <>
-            <Header />
+        <div className="text-secondary bg-primary">
+            <Header navLinks={NAV_LINKS}/>
             <main>
                 {children}
             </main>
-        </>
+            <NewsletterForm />
+            <Footer navLinks={NAV_LINKS}/>
+        </div>
     )
 }
+
+//TODO: change add getLayout function as a property of layout and then bind statically to page component instead of as a static function on a perpage basis
 
 //export const getLayout = (page: ReactElement ) => {
 //    <HeaderLayout>{page}</HeaderLayout>
