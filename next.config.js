e75c9042c521c092915e3d5cfd59ea83fe19773f
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2023-02-13 16:59:30 +0200

    Finished support skeleton for i18n
    TODO: 1. add trans tag to text that will need translating.
          2. automatic locale setting
          3. locale changing dropdown in header
          4. fill in the content and complete the translation messages

--- next.config.js
+++ next.config.js
@@ -1,16 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-}
+    reactStrictMode: true,
 
-module.exports = nextConfig
+    i18n: {
+        locales: ["en", "gr", "pseudo"],
+        defaultLocale: "en",
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "via.placeholder.com",
+                port: "",
+                pathname: "/**",
+            },
+        ],
+    },
+};
+
+module.exports = nextConfig;
