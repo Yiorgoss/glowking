533f694cf53736b64cc64ad7612c1e17482a5737
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2023-02-23 14:01:59 +0200

    finished redesign of service adn portfolio pages
    included data placeholder text
    TODO: real data must still be created

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -3,9 +3,31 @@ import Image from 'next/image'
 
 import { ReactElement } from "react"
 
-import HeaderLayout from "@layouts/headerLayout"
+import LandingLayout from "@layouts/landingLayout"
 import type { PageWithHeaderLayout } from "@cTypes/layoutTypes";
 
+
+const ImageGallery = [
+    {
+        src:"https://via.placeholder.com/200x300",
+    },
+    {
+        src:"https://via.placeholder.com/200x300",
+    },
+    {
+        src:"https://via.placeholder.com/200x300",
+    },
+    {
+        src:"https://via.placeholder.com/200x300",
+    },
+    {
+        src:"https://via.placeholder.com/200x300",
+    },
+    {
+        src:"https://via.placeholder.com/200x300",
+    },
+
+]
 
 const Portfolio: PageWithHeaderLayout = () => {
     //TODO: if masonry layout wanted height and width must be calculated onload
@@ -13,37 +35,37 @@ const Portfolio: PageWithHeaderLayout = () => {
     //      https://stackoverflow.com/questions/66353475/how-to-use-image-component-in-next-js-with-unknown-width-and-height
 
     return (
-        <div className=" container mx-auto mt-10">
-            <h1 className="text-4xl font-bold text-center">Portfolio</h1>
-            <div className='grid gap-6 grid-cols-3 p-8 '>
-                <div className='relative w-full h-[300px] rounded-md overflow-hidden '>
-                    <Image src="https://via.placeholder.com/200x300.png" alt="" fill />
+        <div className=" container mx-auto mt-[100px]">
+            <h1 className="text-6xl tracking-wider font-bold text-center">Our <span className="text-tertiary">Portfolio</span></h1>
+                <div className='grid gap-6 grid-cols-3 p-8 '>
+                    <div className='relative w-full h-[300px] rounded-md overflow-hidden '>
+                        <Image src="https://via.placeholder.com/200x300.png" alt="" fill />
+                    </div>
+                    <div className='relative w-full h-[300px] rounded-md overflow-hidden '>
+                        <Image src="https://via.placeholder.com/200x300.png" alt="" fill />
+                    </div>
+                    <div className='relative w-full h-[300px] rounded-md overflow-hidden '>
+                        <Image src="https://via.placeholder.com/200x300.png" alt="" fill />
+                    </div>
+                    <div className='relative w-full h-[300px] rounded-md overflow-hidden '>
+                        <Image src="https://via.placeholder.com/200x300.png" alt="" fill />
+                    </div>
+                    <div className='relative w-full h-[300px] rounded-md overflow-hidden '>
+                        <Image src="https://via.placeholder.com/200x300.png" alt="" fill />
+                    </div>
+                    <div className='relative w-full h-[300px] rounded-md overflow-hidden '>
+                        <Image src="https://via.placeholder.com/200x300.png" alt="" fill />
+                    </div>
+                    <div className='relative w-full h-[300px] rounded-md overflow-hidden '>
+                        <Image src="https://via.placeholder.com/200x300.png" alt="" fill />
+                    </div>
                 </div>
-                <div className='relative w-full h-[300px] rounded-md overflow-hidden '>
-                    <Image src="https://via.placeholder.com/200x300.png" alt="" fill />
-                </div>
-                <div className='relative w-full h-[300px] rounded-md overflow-hidden '>
-                    <Image src="https://via.placeholder.com/200x300.png" alt="" fill />
-                </div>
-                <div className='relative w-full h-[300px] rounded-md overflow-hidden '>
-                    <Image src="https://via.placeholder.com/200x300.png" alt="" fill />
-                </div>
-                <div className='relative w-full h-[300px] rounded-md overflow-hidden '>
-                    <Image src="https://via.placeholder.com/200x300.png" alt="" fill />
-                </div>
-                <div className='relative w-full h-[300px] rounded-md overflow-hidden '>
-                    <Image src="https://via.placeholder.com/200x300.png" alt="" fill />
-                </div>
-                <div className='relative w-full h-[300px] rounded-md overflow-hidden '>
-                    <Image src="https://via.placeholder.com/200x300.png" alt="" fill />
-                </div>
-            </div>
         </div>
     )
 }
 
 Portfolio.getLayout = function getLayout(page: ReactElement) {
-    return <HeaderLayout>{page}</HeaderLayout>
+    return <LandingLayout>{page}</LandingLayout>
 }
 
 export default Portfolio;
--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -1,38 +1,72 @@
 import Link from "next/link";
-import HeaderLayout from "@layouts/headerLayout";
+import Image from "next/image";
+import LandingLayout from "@layouts/landingLayout";
 
 import { ReactElement } from "react";
 
+import ServiceSection from "@components/serviceSection";
 import CardMain from "@components/cardMain";
 import Button from "@components/common/button/button";
 import type { PageWithHeaderLayout } from "@cTypes/layoutTypes";
 
+const SERVICES_LIST = [
+    {
+        title: "Service",
+        content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae erat luctus, venenatis tortor sit amet, aliquam ipsum.Pellentesque habitant morbi tristique senectus et netus etmalesuada fames ac Lorem ipsum dolor sit amet, consecteturadipiscing elit. Proin vitae erat luctus, venenatis tortor sitamet, aliquam ipsum. Pellentesque habitant morbi tristiquesenectus et netus et malesuada fames ac",
+        image: "https://via.placeholder.com/1000x1000.png",
+    },
+    {
+        title: "Service",
+        content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae erat luctus, venenatis tortor sit amet, aliquam ipsum.Pellentesque habitant morbi tristique senectus et netus etmalesuada fames ac Lorem ipsum dolor sit amet, consecteturadipiscing elit. Proin vitae erat luctus, venenatis tortor sitamet, aliquam ipsum. Pellentesque habitant morbi tristiquesenectus et netus et malesuada fames ac",
+        image: "https://via.placeholder.com/1000x1000.png",
+    },
+    {
+        title: "Service",
+        content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae erat luctus, venenatis tortor sit amet, aliquam ipsum.Pellentesque habitant morbi tristique senectus et netus etmalesuada fames ac Lorem ipsum dolor sit amet, consecteturadipiscing elit. Proin vitae erat luctus, venenatis tortor sitamet, aliquam ipsum. Pellentesque habitant morbi tristiquesenectus et netus et malesuada fames ac",
+        image: "https://via.placeholder.com/1000x1000.png",
+    },
+    {
+        title: "Service",
+        content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae erat luctus, venenatis tortor sit amet, aliquam ipsum.Pellentesque habitant morbi tristique senectus et netus etmalesuada fames ac Lorem ipsum dolor sit amet, consecteturadipiscing elit. Proin vitae erat luctus, venenatis tortor sitamet, aliquam ipsum. Pellentesque habitant morbi tristiquesenectus et netus et malesuada fames ac",
+        image: "https://via.placeholder.com/1000x1000.png",
+    },
+];
 const Services: PageWithHeaderLayout = () => {
     return (
-        <div className="container mx-auto mt-10 ">
-            <div className="grid grid-cols-3 gap-y-10">
-                <div className="col-span-2 mx-auto">
-                    <h2 className="text-lg font-semibold">Book a wash</h2>
-                    <div className=" mt-5 w-[600px]">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Proin vitae erat luctus, venenatis tortor sit amet,
-                        aliquam ipsum. Pellentesque
-                    </div>
+        <div className="mt-[100px]">
+            <div className="">
+                <div className="relative h-[300px] w-screen ">
+                    <Image
+                        src="https://via.placeholder.com/1000x400.png"
+                        alt="banner"
+                        fill
+                    />
+                    <h2 className="absolute inset-x-0 bottom-0 pb-20  text-center text-4xl font-bold tracking-wider">
+                        Our <span className="text-tertiary">Services</span>
+                    </h2>
                 </div>
-                <div className="flex flex-wrap content-center justify-center">
-                    <Button href="" text="Book Now" />
-                </div>
-                <CardMain />
-                <CardMain />
-                <CardMain />
-                <CardMain />
+            </div>
+            <div className="mt-10">
+                <ul className="container mx-auto">
+                    {SERVICES_LIST.map((service, i) => (
+                        <ServiceSection
+                            isLeft={i % 2 == 1 ? true : false}
+                            key={i}
+                            {...service}
+                        />
+                    ))}
+                </ul>
             </div>
         </div>
     );
 };
 
 Services.getLayout = function getLayout(page: ReactElement) {
-    return <HeaderLayout>{page}</HeaderLayout>;
+    return <LandingLayout>{page}</LandingLayout>;
 };
 
 export default Services;
